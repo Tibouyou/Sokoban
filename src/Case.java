34cebe421ea7564dccba7febc1bb5d5d304ec00f
Tibouyou <tibouyou01@gmail.com>
2024-04-11 12:20:33 +0200

    Case

---
+++ src/Case.java
@@ -0,0 +1,28 @@
+public class Case {
+    private int x;
+    private int y;
+    public Case() {
+        this.x = 0;
+        this.y = 0;
+    }
+    public Case(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public void move(Direction d) {
+        switch (d) {
+            case UP:
+                this.y--;
+                break;
+            case DOWN:
+                this.y++;
+                break;
+            case LEFT:
+                this.x--;
+                break;
+            case RIGHT:
+                this.x++;
+                break;
+        }
+    }
+}
