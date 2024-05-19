b6511320800062ec93708c229a74117e6165612c
Clara <clara.sithidej@etu.univ-lyon1.fr>
2024-05-19 15:30:16 +0200

    sensor image

--- src/MF.java
+++ src/MF.java
@@ -24,7 +24,7 @@ public class MF extends JFrame implements Observer {
     private BufferedImage background = ImageIO.read(new File("data/background.png"));
     private BufferedImage player = ImageIO.read(new File("data/player0.png"));
     private BufferedImage box = ImageIO.read(new File("data/box.png"));
-    private BufferedImage sensor;
+    private BufferedImage sensor = ImageIO.read(new File("data/sensor.png"));
 
 
     public MF(Grid g) throws IOException {
@@ -64,7 +64,6 @@ public class MF extends JFrame implements Observer {
         for (int i = 0; i < L; i++) {
             for (int j = 0; j < H; j++) {
                 tabC[i][j] = new JPanel();
-                tabC[i][j].setBorder(BorderFactory.createLineBorder(Color.BLACK));
                 tabC[i][j].setBackground(Color.WHITE);
                 jpC.add(tabC[i][j]);
             }
@@ -103,21 +102,6 @@ public class MF extends JFrame implements Observer {
     private void drawGame() {
         for (int i = 0; i < H; i++) {
             for (int j = 0; j < L; j++) {
-                if (g.getCase(i, j) instanceof Wall) {
-                    tabC[g.getCase(i, j).getY()][g.getCase(i, j).getX()].setBackground(Color.BLACK);
-                } else if (g.getCase(i, j) instanceof Sensor) {
-                    tabC[g.getCase(i, j).getY()][g.getCase(i, j).getX()].setBackground(Color.BLUE);
-                } else if (g.getCase(i, j) instanceof Air) {
-                    tabC[g.getCase(i, j).getY()][g.getCase(i, j).getX()].setBackground(Color.WHITE);
-                    /*String coords = g.getCase(i, j).getY() + " " + g.getCase(i, j).getX();
-                    JLabel jlabel = new JLabel(coords);
-                    jlabel.setFont(new Font("Verdana",1,20));
-                    tabC[g.getCase(i, j).getY()][g.getCase(i, j).getX()].add(jlabel);*/
-                }
-            }
-        }
-        for (int i = 0; i < H; i++) {
-            for (int j = 0; j < L; j++) {
                 tabC[j][i].removeAll();
                 tabC[j][i].updateUI();
                 if (g.getEntity(i, j) != null) {
@@ -133,6 +117,21 @@ public class MF extends JFrame implements Observer {
                         tabC[g.getEntity(i, j).getY()][g.getEntity(i, j).getX()].add(new JLabel(icon));
                         setVisible(true);
                     }
+                }
+            }
+        }
+
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < L; j++) {
+                if (g.getCase(i, j) instanceof Wall) {
+                    tabC[g.getCase(i, j).getY()][g.getCase(i, j).getX()].setBackground(Color.BLACK);
+                } else if (g.getCase(i, j) instanceof Sensor) {
+                    Image newSensorSize = sensor.getScaledInstance((int) (caseSize*0.75), (int) (caseSize*0.75), Image.SCALE_SMOOTH);
+                    ImageIcon icon = new ImageIcon(newSensorSize);
+                    tabC[g.getCase(i, j).getY()][g.getCase(i, j).getX()].add(new JLabel(icon));
+                    setVisible(true);
+                } else if (g.getCase(i, j) instanceof Air) {
+                    tabC[g.getCase(i, j).getY()][g.getCase(i, j).getX()].setBackground(Color.WHITE);
                 }
             }
         }
